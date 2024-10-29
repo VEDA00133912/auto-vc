ccdfe372b31df6c2aa9041767878e558cbc5422b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-10-29 13:31:24 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config(); 
+const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} is Online!`);
+});
+
+const loadEvents = (client) => {
+    const eventsPath = path.join(__dirname, 'events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+        const event = require(path.join(eventsPath, file));
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+    }
+};
+
+const loadCommands = (client) => {
+    client.commands = new Map();
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(path.join(commandsPath, file));
+        client.commands.set(command.data.name, command);
+    }
+};
+
+loadEvents(client);
+loadCommands(client);
+
+client.login(process.env.token);
